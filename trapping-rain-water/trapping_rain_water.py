12cc4a67daa092af701816fa8e136649be809b0a
Shruti Tanwar <shruti.tanwar93@gmail.com>
2020-12-20 15:56:22 +0530

    Added trapping_rain_water.py

---
+++ trapping-rain-water/trapping_rain_water.py
@@ -0,0 +1,44 @@
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+# Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+#Approach 1
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        water = 0
+        for i in range(1, len(height)-1):
+            max_left = height[i]
+            
+            for j in range(i):
+                max_left = max(max_left, height[j])
+                
+            max_right = height[i]
+            for j in range(i+1, len(height)):
+                max_right = max(max_right, height[j])
+                
+            water = water + (min(max_left, max_right) - height[i])
+        return water
+        
+        
+        
+#Approach 2
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) == 0:
+            return 0
+        water = 0
+        left = [0]*len(height)
+        right = [0]*len(height)
+        left[0] = height[0]
+        for i in range(1, len(height)):
+            left[i] = max(left[i-1], height[i])
+            
+        right[len(height)-1] = height[len(height)-1]
+        for i in range(len(height)-2, -1, -1):
+            right[i] = max(right[i+1], height[i])
+        
+        for i in range(len(height)):
+            water = water + (min(left[i], right[i]) - height[i])
+        return water
