fd2c844fcb1692d2b2c260fe074fce5bda65f095
Shruti Tanwar <shruti.tanwar93@gmail.com>
2020-12-20 19:23:42 +0530

    Updated longest_substring.py

--- longest-substring/longest_substring.py
+++ longest-substring/longest_substring.py
@@ -5,16 +5,43 @@
 #Output: 3
 #Explanation: The answer is "abc", with the length of 3.
 
+#approach 1
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 1:
+        if len(s) == 0:
             return 0
+        if len(s) == 1:
+            return 1
         sub = s[0]
-        for idx, ch in enumerate(s):
-            while idx < len(s)-1:
-                if ch != s[idx+1] and ch not in sub:
-                    sub = sub+s[idx+1]
-                else:
-                    sub = s[idx+1]
-        print(sub)
+        length_counter = 0
+        for idx in range(1, len(s)):
+            if s[idx] not in sub:
+                sub = sub+s[idx]
+                length_counter = len(sub)
+            else:
+                if len(sub)> length_counter:
+                    length_counter = len(sub)
+                sub = s[idx]
+        return length_counter
+    
+    
+#approach 2 (sliding window)
+n = len(s)
+        ans = 0
+        mp = {}
+
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):
+            if s[j] in mp:
+                i = max(mp[s[j]], i)
+                print("i",i)
+
+            ans = max(ans, j - i + 1)
+            print("ans", ans)
+            mp[s[j]] = j + 1
+            print(mp)
+
+        return ans
+                
                 
