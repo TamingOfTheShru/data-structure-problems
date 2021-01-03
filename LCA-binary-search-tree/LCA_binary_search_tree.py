3869ce10a76be111d4bee4f34cb10f6b5485f368
Shruti Tanwar <shruti.tanwar93@gmail.com>
2021-01-03 19:52:07 +0530

    Added LCA_binary_search_tree.py

---
+++ LCA-binary-search-tree/LCA_binary_search_tree.py
@@ -0,0 +1,21 @@
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return -1
+        while root != None:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                break
+        return root
