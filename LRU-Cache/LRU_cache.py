d74d8f626999441e5d17bf66559ba9332cec8720
Shruti Tanwar <shruti.tanwar93@gmail.com>
2021-01-20 00:46:43 +0530

    Added LRU_cache.py

---
+++ LRU-Cache/LRU_cache.py
@@ -0,0 +1,29 @@
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+#approach 1, using ordered_dict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache.get(key)
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+#Your LRUCache object will be instantiated and called as such:
+#obj = LRUCache(capacity)
+#param_1 = obj.get(key)
+#obj.put(key,value)
+
+# ["LRUCache","put","put","get","put","get","put","get","get","get"]
+# [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
