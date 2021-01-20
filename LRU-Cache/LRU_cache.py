fde832fa5080959b2a125fc8c927208b7897f917
Shruti Tanwar <shruti.tanwar93@gmail.com>
2021-01-20 11:43:58 +0530

    Added approach 2 for LRU cache

--- LRU-Cache/LRU_cache.py
+++ LRU-Cache/LRU_cache.py
@@ -20,6 +20,40 @@ class LRUCache:
         if len(self.cache) > self.capacity:
             self.cache.popitem(last=False)
 
+# approach 2, using deque            
+from collections import deque
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.q = deque()
+        self.timestamp = 0
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            item = self.cache[key]
+            item[1] = self.timestamp
+            print(item)
+            self.q.append((self.timestamp, key))
+            self.timestamp+=1
+            return item[0]
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache and len(self.cache) >= self.capacity:
+            while True:
+                t, k = self.q.popleft()
+                print(t,k)
+                if self.cache[k][1] == t:
+                    break
+                self.cache.pop(k)
+        self.cache[key] = [value, self.timestamp]
+        self.q.append((self.timestamp, key))
+        
+        self.timestamp+=1
+
 #Your LRUCache object will be instantiated and called as such:
 #obj = LRUCache(capacity)
 #param_1 = obj.get(key)
